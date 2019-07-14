7a66830df72d8661e123c76935d3c5bba7c561bd
David Lee <davidchanholee@gmail.com>
2019-07-14 19:47:45 -0400

    Added user class
    Added user class and declarations to it

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    userid:number;
+    firstname:String;
+    lastname:String;
+    username:String;
+    password:String;
+    email:String;
+    bodyweight:number;
+    gender:String;
+}
