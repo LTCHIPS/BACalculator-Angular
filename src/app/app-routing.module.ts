a76d1f18b20bf2f3cf2271fa1f2f8d8900fd0dc1
LTCHIPS <ltchips994@gmail.com>
2019-07-14 17:38:32 -0400

    we made a login component with stuff in it :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+
+//path: URL to map to | component: component that goes with URL
+const routes: Routes = [
+  {path: 'login', component : LoginComponent},
+  {path: 'register', component : RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +9,15 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  @Input()
+  username : string;
+
+  @Input()
+  password : string;
+
   ngOnInit() {
   }
+
+
 
 }
